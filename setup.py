a4b2061ae6284057bbfdaed5407bf11b546a67ce
Marc Abramowitz <marc@marc-abramowitz.com>
2015-03-31 23:26:49 -0700

    setup.py: Correct spelling of Philips
    and update description slightly.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
     version='0.0.1.dev0',
-    description='Manages the phillips hue lightbulbs',
+    description='Manages Philips Hue light bulbs',
     long_description=long_description,
     url='https://github.com/sontek/bulby.git',
     author='John Anderson',
